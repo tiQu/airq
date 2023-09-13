5e8d8200f5a4cb49aca7d7a61fad73d402f744db
allrightyougotme@gmail.com <allrightyougotme@gmail.com>
2023-09-12 20:01:07 -0700

    reading local file for areacode to pass in

---
+++ airq.hs
@@ -0,0 +1,45 @@
+#!/usr/bin/env cabal
+{- cabal:
+build-depends: base, aeson, text, bytestring, network-uri, http-client, http-client-tls
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import qualified Network.HTTP.Client            as H
+import qualified Network.HTTP.Client.TLS        as H
+import Network.URI (escapeURIString)
+import GHC.Generics
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import qualified Data.ByteString.Lazy as B
+
+data AreaCode = AreaCode { areacode :: String } deriving (Show, Generic)
+instance FromJSON AreaCode where
+  parseJSON (Object v) = AreaCode
+                         <$> (v .: "areacode")
+
+-- tQ: TODO define main function, use map function list to apply over API results, return and render result
+
+main :: IO ()
+main = do
+  inputFile <- B.readFile "areacode.json"
+  let ma = decode inputFile :: Maybe AreaCode
+
+  case ma of
+    Nothing -> print "Error parsing local JSON file"
+    Just a -> (putStrLn.queryAirQuality) a
+
+queryAirQuality a = (show.areacode) a
+
+{-
+queryAirQuality :: IO ()
+queryAirQuality a = do
+  httpman <- H.newManager H.tlsManagerSettings
+  --localFile.location
+  let encodedQueryString = H.parseRequest ("https://api.weather.gc.ca/collections/aqhi-forecasts-realtime/items/AQ_FCST-" ++ a.areacode ++ "-202309030000-202309030000?f=json")
+  let req = H.setQueryString [("q", Just "r")] encodedQueryString
+  response <- H.httpLbs req httpman
+  print response
+-}
